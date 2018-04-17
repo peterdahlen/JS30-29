73aa203df4e8a63b68c739ee14f33bfdd622e91e
Peter Dahlén <peterjandahlen@gmail.com>
2018-04-17 13:59:00 +0200

    KLAR!

--- scripts-START.js
+++ scripts-START.js
@@ -41,3 +41,11 @@ function displayEndTime(timestamp) {
   }
 
   buttons.forEach(button => button.addEventListener('click', startTimer));
+
+  document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+  });
+  
