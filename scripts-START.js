2c256739ebcf7a9943a453aee7c31bb05afab409
Peter Dahlén <peterjandahlen@gmail.com>
2018-04-17 13:48:39 +0200

    function displayEndTime

--- scripts-START.js
+++ scripts-START.js
@@ -1,9 +1,13 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
     
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -16,5 +20,22 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-    console.log(seconds);
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    endTime.textContent = `Var tillbaka kl ${hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  }
+
+  function startTimer() {
+      console.log(this.dataset.time);
+  }
+
+  buttons.forEach(button => button.addEventListener('click', startTimer));
